c6f18e50dca571ed11400e22222577bd8f65b909
Michael Raheem <abiolar544@gmail.com>
2022-06-06 16:16:26 +0100

    modified

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import random
 
 user_action = input("Enter a choice \n R for rock\n P for paper\n S for scissors\n ")
+user_action = user_action.upper()
 possible_actions = ["R", "P", "S"]
 computer_action = random.choice(possible_actions)
 print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
 
-if user_action == computer_action:
-    print(f"Both players selected {user_action}. It's a tie!")
-elif user_action == "R":
-    if computer_action == "S":
-        print("Rock smashes scissors! You win!")
-    else:
-        print("Paper covers rock! You lose.")
-elif user_action == "P":
-    if computer_action == "R":
-        print("Paper covers rock! You win!")
-    else:
-        print("Scissors cuts paper! You lose.")
-elif user_action == "S":
-    if computer_action == "P":
-        print("Scissors cuts paper! You win!")
-    else:
-        print("Rock smashes scissors! You lose.")
+def main():
+    if user_action == computer_action:
+        print(f"Both players selected {user_action}. It's a tie!")
+        main()
+    elif user_action == "R":
+        if computer_action == "S":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_action == "P":
+        if computer_action == "R":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == "S":
+        if computer_action == "P":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
+
+main()
